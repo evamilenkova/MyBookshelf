67f7d8f26f78e1a99ad395a38be7d0323bcab266
evamilenkova <emilenkova@yahoo.com>
2023-06-21 00:34:42 +0200

    host added

--- final_project/settings.py
+++ final_project/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-)$%gjhbudr#*+6fl2lr-rv_*k7#y8^*k(be4luj3lh0k##hdia
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['evamilenkova.pythonanywhere.com']
 
 # Application definition
 
